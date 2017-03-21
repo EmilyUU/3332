edf67f65d769896346f8fde19ac6f79b11241ef1
WINDOWS-MC58R68\TCLDUSER <yyf40989@ly.com>
2017-03-21 18:22:45 +0800

    rr

---
+++ test1/src/unit/w.java
@@ -0,0 +1,14 @@
+package unit;
+import demo.calu;
+import org.testng.annotations.Test;
+/**
+ * Created by TCLDUSER on 2017/3/21.
+ * uuuu
+ */
+public class w {
+    @Test
+    public void testcal1(){
+        calu test=new calu();
+        test.cal2();
+    }
+}
